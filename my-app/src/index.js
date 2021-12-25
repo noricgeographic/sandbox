d68d227a69a9b6f8aac2793da0d5f52576941a1f
noric <info@noricgepgraphic.com>
2021-12-25 16:49:45 +0900

    inline CSS

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,5 @@
 import ReactDom from "react-dom";
+import { App } from "./App";
 
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-}
 
 ReactDom.render(<App />, document.getElementById("root"));
