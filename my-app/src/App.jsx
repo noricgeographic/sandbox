6fa0269f3b7a57600d5a01b73530baf74b580675
noric <info@noricgepgraphic.com>
2021-12-26 10:38:11 +0900

    useState

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,18 @@
-import {ColoredMessage} from "./components/ColoredMessage";
+import { ColoredMessage } from "./components/ColoredMessage";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const App = () => {
+console.log("レンダリング");
+
+  // stateの定義
+  const [num, setNum] = useState(0);
 
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   }
+
+  useEffect(() => {alert()}, [num]);
 
   const contentStyle = {
     color: "blue",
@@ -17,6 +25,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 }
