ba61a82aa5581a67abba1b484b2801f323081534
noric <info@noricgepgraphic.com>
2021-12-26 21:04:28 +0900

    emotion

--- my-app/src/components/Emotion.jsx
+++ my-app/src/components/Emotion.jsx
@@ -1,8 +1,10 @@
 /** @jsxImportSource @emotion/react */
-import { jsx, css } from "@emotion/react";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled"
 
 export const Emotion = () => {
 
+  // scssの書き方がそのまま可能な書き方
   const containerStyle = css`
     border: solid 1px #aaa;
     border-radius: 20px;
@@ -10,10 +12,21 @@ export const Emotion = () => {
     margin: 8px;
   `;
 
+  // インラインスタイルの書き方
+  const titleStyle = css({
+    margin: 0,
+    color: "red"
+  });
+
+  // styled-componentsの書き方
+  const SButton = styled.button`
+    background-color: black;
+  `
+
   return (
     <div css={containerStyle}>
-      <p>Emotionです</p>
-      <button>ボタン</button>
+      <p css={titleStyle}>Emotionです</p>
+      <SButton>ボタン</SButton>
     </div>
   )
  }
