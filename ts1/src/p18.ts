7338943390d77388466343bfa9fbcbbf8ecf057f
noric <info@noricgepgraphic.com>
2021-12-29 00:39:01 +0900

    string number boolean

---
+++ ts1/src/p18.ts
@@ -0,0 +1,23 @@
+console.log("start");
+
+let boo1 = true;
+const boo2 = false;
+
+console.log(boo1);
+console.log(boo2);
+
+let num1 = 9007199254740991;
+const num2 = -9007199254740991;
+
+console.log(num1);
+console.log(num2);
+
+let str1 = "あいうえお";
+const str2 = "アイウエオ";
+
+console.log(str1);
+console.log(str2);
+
+let boo3: boolean = true;
+let num3: number = 123;
+let str3: string = "aiueo";
