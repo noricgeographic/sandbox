f4349ef4e0138bead3d33931e0aeb6d0115634d5
noric <info@noricgepgraphic.com>
2021-12-29 11:21:57 +0900

    array tuple

---
+++ ts1/src/p35-array.ts
@@ -0,0 +1,14 @@
+let arrayA = [1, 2, 3];
+let arrayB: number[] = [2, 3, 4];
+let arrayC: Array<number> = [3, 4, 5];
+
+console.log(arrayA);
+console.log(arrayB);
+console.log(arrayC);
+
+let tuppleA: [string, string, number] = ["液晶ビエラ", "Sony", 120000];
+let tuppleB = ["液晶ビエラ", "Sony", 120000];
+let tuppleC: [string, number, boolean] = ["液晶ビエラ", 120000, false];
+console.log(tuppleA);
+console.log(tuppleB);
+console.log(tuppleC);
